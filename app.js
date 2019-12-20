0943886d9d6b66c5f43409c73899b7842e689643
Luigi LLanos <luigillanos@luigis-mbp.home>
2019-12-20 17:49:36 -0500

    started structure

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+//BUDGET CONTROLLER  -------------------------
+var budgetController = (function(){
+ 
+    //some code
+    
+    })();
+    
+    
+    //UI CONTROLLER ------------------------------
+    var UIController = (function(){
+    
+    // some code later
+    
+    })();
+    
+    
+    
+    //GLOBAL APP CONTROLLER  ----------------------
+    var controller = (function(budgetCtrl, UICtrl){
+    
+        //set event listener
+        document.querySelector('.add__btn').addEventListener('click', function(){
+            
+            //1. Get field input data
+    
+            //2. Add the item to the budget controller
+    
+            //3. Add the item to UI
+    
+            //4. Calcualte budget
+    
+            //5. Display the bidget on the UI
+            
+        })
+     
+    
+    })(budgetController, UIController);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
